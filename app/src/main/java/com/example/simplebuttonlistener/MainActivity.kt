dd51e7ab6d4040fac7ef72b75c599a17bd354a7c
d7d7s <d7d7sultan@gmail.com>
2021-10-17 11:57:29 +0300

    Himah-mobile-development-bootcamp-android

--- app/src/main/java/com/example/simplebuttonlistener/MainActivity.kt
+++ app/src/main/java/com/example/simplebuttonlistener/MainActivity.kt
@@ -7,9 +7,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -18,29 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val xmlButton = findViewById<Button>(R.id.xml_btn)
-        xmlButton.setOnClickListener {
-            Toast.makeText(this, "XML button was clicked", Toast.LENGTH_LONG).show()
+        val input = findViewById<EditText>(R.id.et_input)
+        val btn = findViewById<Button>(R.id.btn_submit)
+        val display = findViewById<TextView>(R.id.tv_display)
+
+        btn.setOnClickListener {
+            display.text = input.text
         }
-
-        val height = Resources.getSystem().displayMetrics.heightPixels
-
-        val constraint = findViewById<ConstraintLayout>(R.id.constraint)
-        val kotlinButton = Button(this)
-        kotlinButton.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-        )
-        kotlinButton.text = "Kotlin"
-        kotlinButton.y = (height / 2).toFloat() + 30
-        kotlinButton.setBackgroundColor(Color.CYAN)
-        kotlinButton.gravity = Gravity.CENTER
-
-        kotlinButton.setOnClickListener {
-            Toast.makeText(this, "Kotlin button was clicked", Toast.LENGTH_LONG).show()
-
-        }
-        constraint.addView(kotlinButton)
 
     }
 }
